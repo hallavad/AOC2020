730f9c3528b376ba6745af7a53b3cc4710628d94
Lucas Glimfjord <lucas@glimfjord.com>
2020-12-01 21:39:59 +0100

    Added some comments and an alternate solution to part a

--- 1.py
+++ 1.py
@@ -1,20 +1,62 @@
 from aocd import data, submit
 
+day = 1
+submit = True
+
+def submit_a(res):
+    submit(res, part="a", day=day, year=2020)
+
+def submit_b(res):
+    submit(res, part="b", day=day, year=2020)
+
+
+# Process the input data into a list
 data_list = list(map(int, data.split("\n")))
 
-def two_sum_2020(d):
+
+
+
+
+
+# Part a
+# O(N^2)
+def part_a(d):
     for i in range(len(d)):
-        for j in range(i, len(d)):
+        for j in range(i+1, len(d)):
             if d[i]+d[j] == 2020:
-                return d[i]*d[j]
-#submit(two_sum_2020(data_list), part="a", day=1, year=2020)
+                print(d[i]*d[j])
+                submit_a(d[i]*d[j])
 
+def alternate_part_a(d):
+    # sort the list
+    d.sort()
 
+    remainder = d[0]
 
+    upper = len(d)-1
+    lower = 0
+
+    while upper > lower:
+        # raise lower bound
+        while d[lower]+d[upper] < 2020:
+            lower += 1
+
+        if d[lower]+d[upper] == 2020:
+            submit_a(d[lower]*d[upper])
+            print(d[lower]*d[upper])
+            return True
+
+        upper -= 1
+
+    print("couldn't find a match")
+    return False
+
+# Part b
+# O(N^3)
 def three_sum_2020(d):
     for i in range(len(d)):
-        for j in range(i, len(d)):
-            for k in range(j, len(d)):
+        for j in range(i+1, len(d)):
+            for k in range(j+1, len(d)):
                 if d[i]+d[j]+d[k] == 2020:
-                    return d[i]*d[j]*d[k]
-submit(three_sum_2020(data_list), part="b", day=1, year=2020)
+                    print(d[i]*d[j]*d[k])
+                    submit_b(d[i]*d[j]*d[k])
---
+++ README.md
@@ -0,0 +1,5 @@
+# Day 1
+Simple solution with dual loops for part a and triple loop for part b.
+Since the first value has been checked for all possible alternatives the second value will always start one above the prior one.
+
+The alternate solution of part a makes use of sorting the list and then excluding the values at the end until it finds the correct ones that.
