bd151df413558a750abbcdbac8321ce587898ea7
Lucas Glimfjord <lucas@glimfjord.com>
2020-12-02 09:06:18 +0100

    Did some minor changes to my template

--- 0.py
+++ 0.py
@@ -1,13 +1,23 @@
 from aocd import data, submit
 
-day = 1
+#the current day
+day =
 
+#submit part a
 def submit_a(res):
-    submit(res, part="a", day, year=2020)
+    submit(res, part="a", day=day, year=2020)
 
+# submit part b
 def submit_b(res):
-    submit(res, part="b", day, year=2020)
+    submit(res, part="b", day=day, year=2020)
 
 
 # Process the input data into a list
-data_list = list(map(int, data.split("\n")))
+def process_data():
+    def val_func(x):
+
+        rule, pas = x.split(": ")
+        bounds, key = rule.split(" ")
+        lo, hi = bounds.split("-")
+
+        return (pas, key, int(lo), int(hi))
